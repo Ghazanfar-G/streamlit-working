831688db2fc2fd9f735720cb999b73887111aa65
Ghazanfar-G <ghazanfarfareed011@gmail.com>
2025-01-31 16:50:34 +0500

    Create summarizer.py

---
+++ summarizer.py
@@ -0,0 +1,21 @@
+import os 
+import google.generativeai as gen
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+gen.configure(api_key=os.getenv('api'))
+model=gen.GenerativeModel('gemini-1.5-flash-002')
+st.title("(●`◡`●)Theory summarizer")
+theory=st.text_input("Paste your theory here:")
+
+if st.button("Start"):
+    if not theory:
+        st.warning('Paste your theory first.')
+    else:
+        response=model.generate_content(['''Read the theory,and write summary of theory
+                                 in such a way that a person after reading it gain knowledge about 
+                                 the whole theory.''',theory],stream=True)
+        with st.spinner('Loading'):                         
+          for chunk in response:
+            for words in chunk:
+              st.write(words.text)    
